7ce8d171fce130d4d9f7f260a767a86c5d1cfd8a
PIASH <zmnobab1999@gmail.com>
2025-11-10 13:07:46 +0600

    non primitive object type added and understanding how to define optional type, literal type and access modifiers readonly

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -16,3 +16,45 @@ let piashNameAndRoll: [string, number] = ["Piash", 109];
 piashNameAndRoll[0] = ""; //only string for 0 index , and only number for index 1
 
 let destination: [string, string, number] = ["Dhaka", "Chattogram", 3];
+
+//reference type: object
+
+// const user: {
+//   firstName: string;
+//   middleName: string;
+//   lastName: string;
+// } = {
+//   firstName: "Md",
+//   middleName: "Piash",
+//   lastName: "Islam",
+// };
+// const user: {
+//   organization: "Programming Hero"; //value => type, and this type call literal types
+//   firstName: string;
+//   middleName?: string; //optional type
+//   lastName: string;
+//   isMarried: boolean;
+// } = {
+//   organization: "Programming Hero",
+//   firstName: "Jhankar",
+//   lastName: "Mahbub",
+//   isMarried: true,
+// };
+
+// // user.organization = "Programming Hero Fire" //fixed, so never it will be changed
+// console.log(user);
+const user: {
+  readonly organization: string; //readonly => access modifiers
+  firstName: string;
+  middleName?: string; //optional type
+  lastName: string;
+  isMarried: boolean;
+} = {
+  organization: "Programming Hero",
+  firstName: "Jhankar",
+  lastName: "Mahbub",
+  isMarried: true,
+};
+
+// user.organization = "Programming Hero Fire" //readonly, so never it will be changed
+console.log(user);
