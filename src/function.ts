00413ea4dd5b1a37ce2d34565b0b56b2f639531a
PIASH <zmnobab1999@gmail.com>
2025-11-10 13:23:33 +0600

    declare function in the object and define its type

--- src/function.ts
+++ src/function.ts
@@ -9,3 +9,17 @@ console.log(sumNormal(2, 2));
 
 const sumArrow = (num1: number, num2: number): number => num1 + num2;
 console.log(sumArrow(2, 3));
+
+//object =>  function => method
+
+const poorUser = {
+  name: "Piash",
+  balance: 0,
+  addBalance(value: number): number {
+    const totalBalance = this.balance + value;
+    console.log(totalBalance)
+    return totalBalance;
+  },
+};
+
+poorUser.addBalance(10000)
