09fe4dfacaa5edb1324d46d09518212a6673f45d
PIASH <zmnobab1999@gmail.com>
2025-11-10 14:11:30 +0600

    added object destructuring

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -12,5 +12,17 @@ const user = {
   favoriteColor: "black",
 };
 
-const {favoriteColor: myFavoriteColor, name: {middleName: myMiddleName}} = user;  //name aliasing favoriteColor to myFavoriteColor
-console.log(myFavoriteColor, myMiddleName)
+const {
+  favoriteColor: myFavoriteColor,
+  name: { middleName: myMiddleName },
+} = user; //name aliasing favoriteColor to myFavoriteColor
+// console.log(myFavoriteColor, myMiddleName)
+
+//array destructuring =>
+
+const friends = ["Karim", "Rahim", "Mahim"];
+// const bestFriend = friends[1];
+// console.log(bestFriend);
+
+const [,,myBestFriend]= friends
+console.log(myBestFriend);
