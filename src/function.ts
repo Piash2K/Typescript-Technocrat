91b5ba673b2ae09de2e707e7badf088cde3c66f8
PIASH <zmnobab1999@gmail.com>
2025-11-10 13:18:25 +0600

    type define for normal and arrow function

---
+++ src/function.ts
@@ -0,0 +1,11 @@
+//Function
+
+//two types: arrow  function, normal function
+
+function sumNormal(num1: number, num2: number): number {
+  return num1 + num2;
+}
+console.log(sumNormal(2, 2));
+
+const sumArrow = (num1: number, num2: number): number => num1 + num2;
+console.log(sumArrow(2, 3));
