02f577e9062c63179932c71e05b3168454844586
PIASH <zmnobab1999@gmail.com>
2025-11-11 19:47:04 +0600

    type assertion added

---
+++ src/typeAssertion.ts
@@ -0,0 +1,28 @@
+//type assertion or narrowing
+
+let anything: any;
+
+anything = "Piash";
+
+const kgToGMConverter = (
+  input: string | number
+): string | number | undefined => {
+  if (typeof input === "number") {
+    return input * 1000;
+  } else if (typeof input === "string") {
+    const [value] = input.split(" ");
+    return `Converted output is: ${Number(value) * 1000}`;
+  }
+};
+const result1 = kgToGMConverter(2) as number;
+console.log({ result1 });
+const result2 = kgToGMConverter("2 kg") as string;
+console.log({ result2 });
+
+type CustomError = {
+  message: string;
+};
+try {
+} catch (error) {
+  console.log((error as CustomError).message);
+}
