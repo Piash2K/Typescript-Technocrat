cc2d7872c4e360503e5dde5dd0cab19858f47fcd
PIASH <zmnobab1999@gmail.com>
2025-11-15 21:48:01 +0600

    learning access modifiers

---
+++ src/accessmodifiers.ts
@@ -0,0 +1,64 @@
+// // access >> modify
+
+// class BankAccount {
+//   readonly userId: number;
+//   userName: string;
+//   private userBalance: number;
+
+//   constructor(userId: number, userName: string, userBalance: number) {
+//     this.userId = userId;
+//     this.userName = userName;
+//     this.userBalance = userBalance;
+//   }
+
+//   addBalance(balance: number) {
+//     return (this.userBalance = this.userBalance + balance);
+//   }
+// }
+
+
+
+// const PiashVaierAccount = new BankAccount(222, "Piash", 20);
+
+// PiashVaierAccount.addBalance(20);
+// PiashVaierAccount.addBalance(50);
+
+
+// console.log(PiashVaierAccount)
+
+
+
+//------------///
+
+
+// access >> modify
+
+class BankAccount {
+  readonly userId: number;
+  userName: string;
+  protected userBalance: number;
+
+  constructor(userId: number, userName: string, userBalance: number) {
+    this.userId = userId;
+    this.userName = userName;
+    this.userBalance = userBalance;
+  }
+
+  addBalance(balance: number) {
+    return (this.userBalance = this.userBalance + balance);
+  }
+}
+
+class StudentBankAccount extends BankAccount {
+    test(){
+        this.userBalance
+    }
+}
+
+const PiashVaierAccount = new BankAccount(222, "Piash", 20);
+
+PiashVaierAccount.addBalance(20);
+PiashVaierAccount.addBalance(50);
+
+
+console.log(PiashVaierAccount)
